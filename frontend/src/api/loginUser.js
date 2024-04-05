bf5a7eb87554bd78fb791ff58ce12d541e892013
Abhishek roshan <abhishekroshan11@gmail.com>
2024-04-05 13:31:57 +0530

    complete

--- frontend/src/api/loginUser.js
+++ frontend/src/api/loginUser.js
@@ -16,3 +16,22 @@ export const loginUser = async (email, password) => {
     throw error;
   }
 };
+
+// import axios from "axios";
+
+// export const loginUser = async (email, password) => {
+//   try {
+//     const res = await axios.post("https://task-backend-dvmm.onrender.com/", {
+//       email: email,
+//       password: password,
+//     });
+
+//     localStorage.setItem("token", res.data.token);
+//     localStorage.setItem("userEmail", email);
+
+//     return res;
+//   } catch (error) {
+//     console.log(error);
+//     throw error;
+//   }
+// };
